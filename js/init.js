ee1e48b825970d8932cd92cf6f711017acc3cd34
M.Mukil Karki <mukilkarkimail@gmail.com>
2024-06-26 20:37:27 +0530

    init.js

--- js/init.js
+++ js/init.js
@@ -3,5 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const initialUsers = [
         { username: 'user', password: 'pass', email: 'user@example.com' }
     ];
-    localStorage.setItem('users', JSON.stringify(initialUsers));
+    if (!localStorage.getItem('users')) {
+        localStorage.setItem('users', JSON.stringify(initialUsers));
+    }
 });
